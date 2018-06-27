e2fa57145aedabe9845703283aa36f8e43d56239
MagistrSanich <iamsexysexysexy@gmail.com>
2018-06-27 15:28:30 +0700

    Добавьте файлы проекта.

---
+++ Practic4/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practic4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string Summ()
+        {
+            int a = Int32.Parse(Request.Params["a"]);
+            int b = Int32.Parse(Request.Params["b"]);
+            return "A + B = " + (a + b).ToString() + ".";
+        }
+
+        [HttpGet]
+        public ActionResult GetBook()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string PostBook()
+        {
+            string title = Request.Params["title"];
+            string author = Request.Params["author"];
+            return title + " " + author;
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Practic4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Practic4.MvcApplication" Language="C#" %>
